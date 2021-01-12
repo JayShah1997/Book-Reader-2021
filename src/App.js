cf7126b4543a6319e2b8a398fc12c5f3cadcfe8d
Vinit Raut <vinit.coder@gmail.com>
2021-01-13 00:16:19 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='text-center mx-auto mt-40 mb-8 bg w-7/12'>
+      Hello, World
+    </div>
+  );
+};
+
+export default App;
