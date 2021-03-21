0371a821934eed1d4835c75d370e3097d510cd77
Vinit Raut <vinit.coder@gmail.com>
2021-03-21 22:40:53 +0530

    Add: Audio for home page and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,18 @@
 import React from "react";
+import useSound from "use-sound";
+
+import footerAudio from "../audio/HomeAudio/footerAudio.mp3";
 
 const Footer = () => {
+  const [footerPlay, { stop: footerStop }] = useSound(footerAudio);
+
   return (
     <div className="relative mt-4 h-12 flex justify-center items-center">
-      <p className="text-center text-gray-700 text-xl  hover:text-gray-900 transition duration-300 ">
+      <p
+        className="text-center text-gray-700 text-xl  hover:text-gray-900 transition duration-300"
+        onMouseEnter={() => footerPlay()}
+        onMouseLeave={() => footerStop()}
+      >
         Made with ❤️ by students for students
       </p>
     </div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,23 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import useSound from "use-sound";
+import { Link } from "react-router-dom";
+
+import bookReaderAudio from "../audio/NavbarAudio/book-reader.mp3";
+import titleAudio from "../audio/HomeAudio/titleAudio.mp3";
+import ctaAudio from "../audio/HomeAudio/ctaAudio.mp3";
 
 const Hero = () => {
+  const [bookReaderPlay, { stop: bookReaderStop }] = useSound(bookReaderAudio);
+  const [titlePlay, { stop: titleStop }] = useSound(titleAudio);
+  const [ctaPlay, { stop: ctaStop }] = useSound(ctaAudio);
+
   return (
     <div
-      className='mt-24 flex flex-col justify-center items-center'
-      style={{ minHeight: '70vh' }}
+      className="mt-24 flex flex-col justify-center items-center"
+      style={{ minHeight: "70vh" }}
     >
-      <h1 className='relative text-6xl mb-6 font-bold uppercase text-transparent bg-clip-text bg-gradient-to-r from-purple-500  to-green-500'>
-        <i class='fas fa-book'></i> Book Reader
+      <h1
+        className="relative text-6xl mb-6 font-bold uppercase text-transparent bg-clip-text bg-gradient-to-r from-purple-500  to-green-500"
+        onMouseEnter={() => bookReaderPlay()}
+        onMouseLeave={() => bookReaderStop()}
+      >
+        <i class="fas fa-book"></i> Book Reader
       </h1>
 
-      <p className='relative text-2xl uppercase tracking-wider font-bold text-gray-700 mb-6'>
+      <p
+        className="relative text-2xl uppercase tracking-wider font-bold text-gray-700 mb-6"
+        onMouseEnter={() => titlePlay()}
+        onMouseLeave={() => titleStop()}
+      >
         Education is a gift every kid deserves ✨
       </p>
 
       <Link
-        to='/books'
-        class='relative inline-block mb-16 lg:mb-8 py-3 px-8 rounded bg-gradient-to-br from-purple-500 to-purple-700 text-purple-100 hover:text-white hover:to-purple-600 shadow-lg hover:shadow-2xl font-bold text-lg tracking-wide transition duration-500'
+        to="/books"
+        class="relative inline-block mb-16 lg:mb-8 py-3 px-8 rounded bg-gradient-to-br from-purple-500 to-purple-700 text-purple-100 hover:text-white hover:to-purple-600 shadow-lg hover:shadow-2xl font-bold text-lg tracking-wide transition duration-500"
+        onMouseEnter={() => ctaPlay()}
+        onMouseLeave={() => ctaStop()}
       >
         Discover All Books
       </Link>
