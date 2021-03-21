b2b9356468d807c53db8379371041cc6cca43d2e
Vinit Raut <vinit.coder@gmail.com>
2021-03-21 23:31:45 +0530

    Refactor: English, History and Geography class components to hooks

--- src/components/books/Geography.js
+++ src/components/books/Geography.js
@@ -1,73 +1,69 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PDF from "react-pdf-js";
 
 import geographyPdf from "./geography-1-20.pdf";
 
-class Geography extends Component {
-  state = {};
+const Geography = () => {
+  const [pages, setPages] = useState();
+  const [page, setPage] = useState();
 
-  handlePrevious = this.handlePrevious.bind(this);
-  handleNext = this.handleNext.bind(this);
-  onDocumentComplete = this.onDocumentComplete.bind(this);
+  const onDocumentComplete = (pages) => {
+    setPage(1);
+    setPages(pages);
+  };
 
-  onDocumentComplete(pages) {
-    this.setState({ page: 1, pages });
-  }
+  const handlePrevious = () => {
+    setPage((prevPage) => prevPage - 1);
+  };
 
-  handlePrevious() {
-    this.setState({ page: this.state.page - 1 });
-  }
+  const handleNext = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
 
-  handleNext() {
-    this.setState({ page: this.state.page + 1 });
-  }
-
-  renderPagination() {
+  const renderPagination = () => {
     return (
       <div className="text-center mt-4">
         <button
-          disabled={this.state.page === 1}
+          disabled={page === 1}
           className="mr-4 focus:outline-none bg-gray-300 hover:bg-gray-400 text-gray-700 font-bold py-2 px-4 rounded-l transition-all duration-300 ease-out"
-          onClick={this.handlePrevious}
+          onClick={handlePrevious}
         >
           {" "}
           <i class="fas fa-arrow-circle-left"></i> Prev
         </button>
         <button
-          disabled={this.state.page === this.state.pages}
+          disabled={page === pages}
           className="focus:outline-none bg-gray-300 hover:bg-gray-400 text-gray-700 font-bold py-2 px-4 rounded-r transition-all duration-300 ease-out"
-          onClick={this.handleNext}
+          onClick={handleNext}
         >
           {" "}
           <i class="fas fa-arrow-circle-right"></i> Next
         </button>
       </div>
     );
+  };
+
+  let pagination = null;
+
+  if (pages) {
+    pagination = renderPagination();
   }
 
-  render() {
-    let pagination = null;
-
-    if (this.state.pages) {
-      pagination = this.renderPagination();
-    }
-    return (
-      <div>
-        <div className="text-center mb-1">
-          <span className="font-bold">Geography</span> - Page{" "}
-          <span className="">{this.state.page}</span> of{" "}
-          <span className="">{this.state.pages}</span>
-        </div>
-        <PDF
-          className="border-2 border-blue-400 shadow-lg rounded-lg"
-          file={geographyPdf}
-          page={this.state.page}
-          onDocumentComplete={this.onDocumentComplete}
-        />
-        {pagination}
+  return (
+    <div>
+      <div className="text-center mb-1">
+        <span className="font-bold">Geography</span> - Page{" "}
+        <span className="">{page}</span> of <span className="">{pages}</span>
       </div>
-    );
-  }
-}
+      <PDF
+        className="border-2 border-blue-400 shadow-lg rounded-lg"
+        file={geographyPdf}
+        page={page}
+        onDocumentComplete={onDocumentComplete}
+      />
+      {pagination}
+    </div>
+  );
+};
 
 export default Geography;
