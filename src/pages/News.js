590021c92b46e37770a87b0163a89b556b3ebf62
Vinit Raut <vinit.coder@gmail.com>
2021-03-31 00:18:21 +0530

    Add: Loader component

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Loading from "../components/Loader";
 import newsData from "./../utils/news-data.json";
 
 const responsiveVoice = window.responsiveVoice;
@@ -25,16 +26,20 @@ const News = () => {
     }
   }, []);
 
-  // console.log(news[0]);
-
   if (!news) {
-    return <p>Loading...</p>;
+    return (
+      <div style={{ height: "85vh" }}>
+        <Loading />
+      </div>
+    );
   } else {
-    console.log(news);
-
     return (
       <div style={{ minheight: "85vh" }}>
-        <h1 className="text-6xl text-center my-16 font-bold uppercase text-transparent bg-clip-text bg-gradient-to-r from-purple-500  to-green-500">
+        <h1
+          className="text-6xl text-center my-16 font-bold uppercase text-transparent bg-clip-text bg-gradient-to-r from-purple-500  to-green-500"
+          onMouseEnter={() => responsiveVoice.speak("News")}
+          onMouseLeave={() => responsiveVoice.cancel()}
+        >
           News
         </h1>
 
@@ -61,6 +66,8 @@ const News = () => {
                   className="text-blue-700 hover:text-blue-800 hover:underline transition duration-500 text-sm self-end"
                   href={newsItem.url}
                   target="_blank"
+                  onMouseEnter={() => responsiveVoice.speak("Read More")}
+                  onMouseLeave={() => responsiveVoice.cancel()}
                 >
                   Read More
                 </a>
