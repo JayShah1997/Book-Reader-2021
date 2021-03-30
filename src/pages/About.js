d73ccc4d4031c041982adbdf38a1423f9e37f1d7
Vinit Raut <vinit.coder@gmail.com>
2021-03-31 00:31:42 +0530

    Add: Audio for about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,20 +4,40 @@ import studentImage2 from "./../avatar/jayant.png";
 import studentImage3 from "./../avatar/dipesh.png";
 import professorImage from "./../avatar/professor.png";
 
+const responsiveVoice = window.responsiveVoice;
+
 const About = () => {
   return (
     <div style={{ minHeight: "80vh" }}>
-      <h1 className="text-6xl text-center my-16 font-bold uppercase text-transparent bg-clip-text bg-gradient-to-r from-purple-500  to-green-500">
+      <h1
+        className="text-6xl text-center my-16 font-bold uppercase text-transparent bg-clip-text bg-gradient-to-r from-purple-500  to-green-500"
+        onMouseEnter={() => responsiveVoice.speak("About")}
+        onMouseLeave={() => responsiveVoice.cancel()}
+      >
         About
       </h1>
 
       <div className="flex items-center relative z-10">
         <div className="w-1/2 m-4 mx-16 text-gray-800 leading-relaxed">
           <div className="mb-4">
-            <h3 className="mb-2 text-4xl text-gray-700 font-bold tracking-wide">
+            <h3
+              className="mb-2 text-4xl text-gray-700 font-bold tracking-wide"
+              onMouseEnter={() => responsiveVoice.speak("Our Objective")}
+              onMouseLeave={() => responsiveVoice.cancel()}
+            >
               Our Objective
             </h3>
-            <p className="text-xl">
+            <p
+              className="text-xl"
+              onMouseEnter={() =>
+                responsiveVoice.speak(`
+              Our goal is to bridge the gaps the visually impaired students face
+              to provide a solution which can make knowledge extraction and
+              learning a fun process.
+              `)
+              }
+              onMouseLeave={() => responsiveVoice.cancel()}
+            >
               Our goal is to bridge the gaps the visually impaired students face
               to provide a solution which can make knowledge extraction and
               learning a fun process.
@@ -25,10 +45,25 @@ const About = () => {
           </div>
 
           <div>
-            <h3 className="mb-2 text-4xl text-gray-700 font-bold tracking-wide">
+            <h3
+              className="mb-2 text-4xl text-gray-700 font-bold tracking-wide"
+              onMouseEnter={() => responsiveVoice.speak("How it works?")}
+              onMouseLeave={() => responsiveVoice.cancel()}
+            >
               How it works?
             </h3>
-            <p className="text-xl">
+            <p
+              className="text-xl"
+              onMouseEnter={() =>
+                responsiveVoice.speak(`
+              Students can select books and simply select a page to listen to
+              the page of their choice. The app uses OCR with TTS technology,
+              where data from the books are scanned and converted into audio
+              format.
+            `)
+              }
+              onMouseLeave={() => responsiveVoice.cancel()}
+            >
               Students can select books and simply select a page to listen to
               the page of their choice. The app uses OCR with TTS technology,
               where data from the books are scanned and converted into audio
@@ -46,41 +81,93 @@ const About = () => {
       </div>
 
       <div>
-        <h1 className="text-6xl text-center my-16 font-bold uppercase text-transparent bg-clip-text bg-gradient-to-r from-purple-500  to-green-500">
+        <h1
+          className="text-6xl text-center my-16 font-bold uppercase text-transparent bg-clip-text bg-gradient-to-r from-purple-500  to-green-500"
+          onMouseEnter={() => responsiveVoice.speak("Meet the team")}
+          onMouseLeave={() => responsiveVoice.cancel()}
+        >
           Meet the team
         </h1>
 
         <div className="flex justify-center text-center">
           <div className="shadow-xl rounded-lg border-2 border-green-100 p-12 mb-4">
             <img src={professorImage} alt="" />
-            <h4 className="text-4xl mt-4 font-bold text-gray-800">Ekta Ukey</h4>
-            <p className="text-xl mt-2">Professor at PHCET</p>
+            <h4
+              className="text-4xl mt-4 font-bold text-gray-800"
+              onMouseEnter={() => responsiveVoice.speak("Ekta Ukey")}
+              onMouseLeave={() => responsiveVoice.cancel()}
+            >
+              Ekta Ukey
+            </h4>
+            <p
+              className="text-xl mt-2"
+              onMouseEnter={() => responsiveVoice.speak("Professor at PHCET")}
+              onMouseLeave={() => responsiveVoice.cancel()}
+            >
+              Professor at PHCET
+            </p>
           </div>
         </div>
 
         <div className="mx-16 mt-8 flex justify-between">
           <div className="text-center shadow-xl rounded-lg border border-green-100 p-12">
             <img src={studentImage1} alt="" />
-            <h4 className="text-4xl mt-4 font-bold text-gray-800">
+            <h4
+              className="text-4xl mt-4 font-bold text-gray-800"
+              onMouseEnter={() => responsiveVoice.speak("Aniket Shinde")}
+              onMouseLeave={() => responsiveVoice.cancel()}
+            >
               Aniket Shinde
             </h4>
-            <p className="text-xl mt-2">BE Computer Engineering, PHCET</p>
+            <p
+              className="text-xl mt-2"
+              onMouseEnter={() =>
+                responsiveVoice.speak("B.E Computer Engineering, PHCET")
+              }
+              onMouseLeave={() => responsiveVoice.cancel()}
+            >
+              BE Computer Engineering, PHCET
+            </p>
           </div>
 
           <div className="text-center shadow-xl rounded-lg border border-green-100 p-12">
             <img src={studentImage2} alt="" />
-            <h4 className="text-4xl mt-4 font-bold text-gray-800">
+            <h4
+              className="text-4xl mt-4 font-bold text-gray-800"
+              onMouseEnter={() => responsiveVoice.speak("Jayant Shah")}
+              onMouseLeave={() => responsiveVoice.cancel()}
+            >
               Jayant Shah
             </h4>
-            <p className="text-xl mt-2">BE Computer Engineering, PHCET</p>
+            <p
+              className="text-xl mt-2"
+              onMouseEnter={() =>
+                responsiveVoice.speak("B.E Computer Engineering, PHCET")
+              }
+              onMouseLeave={() => responsiveVoice.cancel()}
+            >
+              BE Computer Engineering, PHCET
+            </p>
           </div>
 
           <div className="text-center shadow-xl rounded-lg border border-green-100 p-12">
             <img src={studentImage3} alt="" />
-            <h4 className="text-4xl mt-4 font-bold text-gray-800">
+            <h4
+              className="text-4xl mt-4 font-bold text-gray-800"
+              onMouseEnter={() => responsiveVoice.speak("Dipesh Parab")}
+              onMouseLeave={() => responsiveVoice.cancel()}
+            >
               Dipesh Parab
             </h4>
-            <p className="text-xl mt-2">BE Computer Engineering, PHCET</p>
+            <p
+              className="text-xl mt-2"
+              onMouseEnter={() =>
+                responsiveVoice.speak("B.E Computer Engineering, PHCET")
+              }
+              onMouseLeave={() => responsiveVoice.cancel()}
+            >
+              BE Computer Engineering, PHCET
+            </p>
           </div>
         </div>
       </div>
